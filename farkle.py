6895aa7d41f062cbf473e2de185fb4b513b5f2c2
jonathan <jnafziger@gmail.com>
2024-09-15 21:49:58 -0400

    working

--- farkle.py
+++ farkle.py
@@ -1,8 +1,9 @@
 from collections import Counter
 from combinations import ScoringCombo, scoring_combos
-from itertools import combinations
+from itertools import combinations_with_replacement
 from typing import Iterable
 import math
+import argparse
 
 def max_score(dice_totals: Counter) -> int:
     """Return the maximum score for the given dice"""
@@ -27,12 +28,34 @@ def probability(dice_totals: Counter, num_rolls: int) -> float:
 
 def enumerate_possible_dice_totals(num_dice: int) -> Iterable[Counter]:
     """Yield all possible dice totals for the given number of dice"""
-    for dice in combinations(range(1, 7), num_dice):
+    for dice in combinations_with_replacement(range(1, 7), num_dice):
         yield Counter(dice)
 
+def scores_and_probs(num_dice: int, banked_score: int = 0) -> dict[int, float]:
+    """Return the expected score for the given number of dice"""
+    results = {}
+    for dice_totals in enumerate_possible_dice_totals(num_dice):
+        score = max_score(dice_totals)
+        prob = probability(dice_totals, num_dice)
+        if score != 0:
+            score += banked_score
+        if score not in results:
+            results[score] = prob
+        else:
+            results[score] += prob
+    return results
+
+
+
 if __name__ == "__main__":
-    dice_totals = list(enumerate_possible_dice_totals(3))
-    for dice_total in dice_totals:
-        assert sum(dice_total.values()) == 3
-    unique_dice_totals = set(tuple(dice_total.most_common()) for dice_total in dice_totals)
-    print(len(unique_dice_totals), len(dice_totals))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num_dice", type=int, help="Number of dice to roll")
+    parser.add_argument("--banked_score", type=int, default=0, help="Banked score")
+    args = parser.parse_args()
+    results = scores_and_probs(args.num_dice, banked_score=args.banked_score)
+    for score, prob in sorted(results.items()):
+        print(f"Score: {score}, Probability: {100*prob:.3f}")
+
+    # expected score
+    expected_score = sum(score * prob for score, prob in results.items())
+    print(f"Expected score: {expected_score:.6f}")
